32add8c049465ef39188c43b65cd7a4f57e6ef54
Aiden Zafar <+>
2023-02-26 17:56:41 -0500

    final submit

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,15 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function onItemFormSubmit(event, NewItem){
+    event.preventDefault()
+    setItems((items) => [...items, NewItem])
+    }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={onItemFormSubmit}/>
     </div>
   );
 }
